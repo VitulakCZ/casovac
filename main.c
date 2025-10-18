8de0c39fe89f7c02e51b038f94211afb55875046
VitulakCZ <stemberk.vit@gmail.com>
2025-10-18 07:49:08 +0200

    Ovládání a paměť

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "raylib.h"
 #define VELIKOST_TEXTU 50
 #define MAX_POCET_CISLIC 5
+#define VYCHOZI_CAS 10
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -20,13 +21,12 @@ int main() {
     while (!WindowShouldClose()) {
         int key = GetCharPressed();
         while (key > 0) {
-            if ((key >= 48) && (key <= 57) && (pocetCislic < MAX_POCET_CISLIC)) {
+            if ((key >= 48) && (key <= 57) && (pocetCislic < MAX_POCET_CISLIC) && !timer) {
                 cisliceSekund[pocetCislic] = (char)key;
                 cisliceSekund[pocetCislic+1] = '\0';
                 pocetCislic++;
             }
             key = GetCharPressed();
-            
         }
         if (IsKeyPressed(KEY_BACKSPACE)) {
             if (pocetCislic > 0) {
@@ -34,17 +34,40 @@ int main() {
                 cisliceSekund[pocetCislic] = '\0';
             }
         }
-        if (IsKeyPressed(KEY_ENTER)) {
+        if (IsKeyPressed(KEY_ENTER) && !timer && pocetCislic != 0) {
             float sekund = atof(cisliceSekundPtr);
             if (sekund != 0) {
                 currentTime = GetTime();
                 endTime = currentTime + sekund;
+                pocetCislic = 0;
                 timer = true;
             }
+            FILE *file;
+            file = fopen("cislo.txt", "w");
+            fprintf(file, cisliceSekundPtr);
+            fclose(file);
+        } else if (IsKeyPressed(KEY_ENTER)) {
+            memset(cisliceSekund, 0, MAX_POCET_CISLIC);
+            timer = false;
         }
         if (IsKeyPressed(KEY_SPACE)) {
+            float sekund = 0;
+            FILE *file;
+            file = fopen("cislo.txt", "r");
+            if (file == NULL) {
+                printf("Soubor cislo.txt neexistuje, výchozí čas je %d sekund.\n", VYCHOZI_CAS);
+                sekund = VYCHOZI_CAS;
+            } else {
+                fscanf(file, "%f", &sekund);
+                fclose(file);
+            }
             currentTime = GetTime();
-            endTime = currentTime + 10.0;
+            if (sekund <= 0) {
+                printf("Soubor cislo.txt je rozbitý, výchozí čas je %d sekund.\n", VYCHOZI_CAS);
+                sekund = VYCHOZI_CAS;
+            }
+            endTime = currentTime + sekund;
+            pocetCislic = 0;
             timer = true;
         }
         BeginDrawing();
@@ -52,6 +75,7 @@ int main() {
             if (timer) {
                 currentTime = GetTime();
                 float remainingTime = endTime - currentTime;
+                float setTime = remainingTime;
                 if (remainingTime <= 0) {
                     timer = false;
                     continue;
